66e5b958efadd7ba4f84d9a360d4596e72586780
bentowski <benjb85@hotmail.fr>
2020-05-13 11:57:51 +0200

    correction d c

--- ft_chars.c
+++ ft_chars.c
@@ -17,10 +17,8 @@ static	void	ft_sgestion(t_flags *flags, int lenght, int opt)
 	char x;
 
 	x = ' ';
-	if (opt == 2)
+	if (opt == 1)
 		x = '0';
-	if (opt == 1 && flags->precision < lenght)
-		flags->precision = 0;
 	if (flags->cutter == 0)
 		if (flags->width > lenght)
 			while (flags->width-- > lenght)
@@ -49,14 +47,16 @@ static	void	ft_special(t_flags *flags, int x)
 	if (flags->precision < 0)
 		flags->precision = lenght;
 	if (flags->neg == 0)
-		ft_sgestion(flags, lenght, 1);
-	if (flags->cutter == 0 || flags->precision >= lenght)
-	{
-		while (x < lenght)
+		ft_sgestion(flags, lenght, 0);
+	if (flags->cutter == 0)
+		while (s[x])
 			ft_write(s[x++], flags);
-	}
+	else if (flags->precision >= lenght)
+		flags->precision = lenght;
+	while (x < flags->precision)
+		ft_write(s[x++], flags);
 	if (flags->neg == 1)
-		ft_sgestion(flags, lenght, 1);
+		ft_sgestion(flags, lenght, 0);
 }
 
 void			ft_s(va_list *list_args, t_flags *flags)
@@ -110,7 +110,7 @@ void 			ft_pourcent(va_list *list_args, t_flags *flags)
 	if (flags->constantep == 1)
 		flags->precision = va_arg(*list_args, int);
 	if (flags->zero == 1 && flags->width > 0)
-		opt = 2;
+		opt = 1;
 	if (flags->width < 0)
 	{
 		flags->neg = 1;
